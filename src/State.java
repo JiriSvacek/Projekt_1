0cd1fce8a734efbc4e23e16fea8b92246b752697
Jiri Svacek <svacek.jiri@email.cz>
2022-08-28 15:37:25 +0200

    Initial commit + create of Class State with atributes

---
+++ src/State.java
@@ -0,0 +1,9 @@
+public class State {
+    private String stateShortcut;
+    private String stateFullName;
+    private float fullVat;
+    private float discountedVat;
+    private boolean specialVat;
+
+
+}
